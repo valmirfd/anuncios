b2ab295d0045a18fe2e4e1850ab089c01d0c325f
Valmir Dornas <fernandocursos2021@gmail.com>
2026-01-28 12:33:28 -0300

    Layout Dashboard

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Controllers\DashboardController;
+use App\Controllers\HomeController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', [HomeController::class, 'index'], ['as' => 'home']);
+
+$routes->group('dashboard', static function ($routes) {
+    $routes->get('/', [DashboardController::class, 'index'], ['as' => 'dashboard']);
+
+   
+});
---
+++ app/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class DashboardController extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Dashboard'
+        ];
+
+        return view('Dashboard/Home/index', $data);
+    }
+}
